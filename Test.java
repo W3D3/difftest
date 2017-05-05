71d35fa444443683714eac9d87136a7810076255
Christoph Wedenig <wedenigc@gmail.com>
2017-05-05 12:27:45 +0200

    deleted, updated, moved and inserted something in Test.java

--- Test.java
+++ Test.java
@@ -2,14 +2,15 @@ package com.test;
 
 public class TestClass extends SuperClass {
 
-  public TestClass()
-  {
-    int var = 123;
-    int toBeDeleted = 5667;
-  }
+  public String newVar = "so new";
 
   private void lol()
   {
     System.out.println("nix");
+  }
+
+  public TestClass()
+  {
+    int varUpd = 4444321;
   }
 }
