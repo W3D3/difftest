a88f241af233c7bad00317668b3593c6a5a1eb16
Christoph Wedenig <wedenigc@gmail.com>
2017-11-27 15:31:17 +0100

    reset Loop again...

--- paper_example1/Loop.java
+++ paper_example1/Loop.java
@@ -1,21 +1,20 @@
 public class Loop {
 
+    private int[] arr;
+
     public Loop(int[] arr) {
         this.arr = arr;
     }
 
     public void doSomething() {
-        MyBubbleSort sorter = new MyBubbleSort();
-        sorter.setSourceArray(this.arr);
-        this.arr = sh.bubbleSort();
 
         System.out.println("Array printed in reverse:");
-        loopReverse();
+        for (int i = this.arr.length - 1; i >= 0; i--) {
+            System.out.println(this.arr[i]);
+        }
     }
 
     private void loopReverse() {
-        for (int i = this.arr.length; i > 0; i--) {
-            System.out.println(this.arr[i]);
-        }
+        //TODO implement
     }
 }
