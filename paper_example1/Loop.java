763f078b40e7aed8e2c4f5d68e1b7ca5adac9280
Christoph Wedenig <wedenigc@gmail.com>
2017-11-27 10:49:03 +0100

    added Loop.java

---
+++ paper_example1/Loop.java
@@ -0,0 +1,16 @@
+public class Loop {
+
+    public Loop(int[] arr) {
+        this.arr = arr;
+    }
+
+    public reverseLoop() {
+        for(int i = this.arr.length - 1; i > 0; i--) {
+            System.out.println(this.arr[i]);
+        }
+    }
+
+    public loopFromTo(int x, int y) {
+        //TODO implement
+    }
+}
