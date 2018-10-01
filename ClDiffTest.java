9c30f3560e55fee5c893216658555a7f139427d0
Christoph Wedenig <christoph@wedenig.org>
2018-10-01 14:57:14 +0200

    reorderd var setting

--- ClDiffTest.java
+++ ClDiffTest.java
@@ -3,8 +3,8 @@ class ClDiffTest {
     public String oldVar = "so old";
 
     public void method() {
-        this.oldVar = "constructor";
         this.oldVar = "newconst";
+        this.oldVar = "constructor";
     }
 
 }
