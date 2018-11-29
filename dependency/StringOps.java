99ffb46bcb9a981a707072941086b1ec2abdb4a9
Christoph Wedenig <christoph@wedenig.org>
2018-11-29 10:50:09 +0100

    added dependencies StringOps

--- dependency/StringOps.java
+++ dependency/StringOps.java
@@ -1,9 +1,16 @@
 package at.aau.difftest;
 
+//useless imports
+import fake.io.PrintWriter;
+import fake.util.Random;
+
+import org.apache.commons.lang3.StringUtils;
+
+
 public class MyApplication {
 	
     public String abbrevate(String src, Integer maxLength) {
         // do some stuff
-        return src;
+        return StringUtils.abbreviate(src, maxLength);
     }
 }
